fa11a3fa763f0341485d0b07a5378caa3ff93de2
JEEWOO SUL <39071676+jeewoo1025@users.noreply.github.com>
2021-02-06 20:51:47 +0900

    재검색 기능 추가

--- app/src/main/java/com/example/proj1_30/MainActivity.java
+++ app/src/main/java/com/example/proj1_30/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.proj1_30;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -13,6 +12,13 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,16 +30,23 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView search_icon, imgPlaces;
-    EditText editSearch;
-    TextView txtSubTitle;
-    RelativeLayout layout;
-    Retrofit flask;
-    RetrofitAPI flaskAPI;
+    private ImageView search_icon, imgPlaces;
+    private EditText editSearch;
+    private TextView txtSubTitle;
 
-    TextView[] txtPlaces = new TextView[MAX];
-    List<String> titleList;
-    static int MAX = 5;
+    private RelativeLayout layout;
+    private Retrofit flask;
+    private RetrofitAPI flaskAPI;
+    ViewPager2 viewPager2;
+    List<String> recommend_list;
+
+    private TextView[] txtPlaces = new TextView[MAX];
+    private List<String> titleList;
+
+    public static final int MAX = 5;
+
+    /////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class);
-
         retrofitAPI.getTitles().enqueue(new Callback<List<String>>(){
             @Override
             public void onResponse(@NonNull Call<List<String>> call, @NonNull Response<List<String>> response){
@@ -69,11 +81,10 @@ public class MainActivity extends AppCompatActivity {
                 .baseUrl("http://3.36.136.219:5000")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
-
         flaskAPI = flask.create(RetrofitAPI.class);
 
-        layout = (RelativeLayout)findViewById(R.id.layout);
+        // View id
+        layout = (RelativeLayout)findViewById(R.id.main_layout);
         search_icon = (ImageView)findViewById(R.id.search_icon);
         editSearch = (EditText)findViewById(R.id.editSearch);
         txtSubTitle = (TextView)findViewById(R.id.txtPlaces);
@@ -85,7 +96,28 @@ public class MainActivity extends AppCompatActivity {
         txtPlaces[2] = (TextView)findViewById(R.id.place2);
         txtPlaces[3] = (TextView)findViewById(R.id.place3);
         txtPlaces[4] = (TextView)findViewById(R.id.place4);
+        viewPager2 = findViewById(R.id.view_pager2);
 
+        // 추천지 5군데 페이지 설정
+        int dpValue = 54;
+        float d = getResources().getDisplayMetrics().density;
+        int margin = (int) (dpValue * d);
+        viewPager2.setPadding(margin, 0, margin, 0);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r*0.15f);
+            }
+        });
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        // 검색 아이콘
         search_icon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -93,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // 검색창 EditText
         editSearch.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int i, KeyEvent keyEvent) {
@@ -117,10 +150,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // 검색어 확인
     public void checkPlace() {
+        viewPager2.setVisibility(View.INVISIBLE);
+
         String txt = editSearch.getText().toString();
         if (txt.length() <= 0 || txt.equals("")) {
-            setPlacesVisible(false, null);
+            setPlacesVisible(false, null, "");
             return;
         }
         txt = txt.trim();
@@ -128,34 +164,34 @@ public class MainActivity extends AppCompatActivity {
         // 비슷한 이름 장소 가져오기
         ArrayList<String> similarPlaces = getSimilarPlaces(txt);
         if(similarPlaces == null) {
-            setPlacesVisible(false, null);
+            setPlacesVisible(false, null, "Places");
             return;
         }
 
         // 장소 보여주기
-        setPlacesVisible(true, similarPlaces);
+        setPlacesVisible(true, similarPlaces, "Places");
     }
 
+    // 비슷한 관광지 받아오기
     public ArrayList<String> getSimilarPlaces(String place) {
         ArrayList<String> value = new ArrayList<>();
 
-        Log.d("AAAA", String.valueOf(titleList.size()));
-        // 비슷한 관광지 받아오기
         for(String name : titleList) {
             if(name.contains(place))
                 value.add(name);
         }
-        Log.d("AAAA", String.valueOf(value.size()));
         return value;
     }
 
-    public void setPlacesVisible(Boolean flag, ArrayList<String> places) {
-        int visibility = View.INVISIBLE;
+    // Places Visible or not
+    public void setPlacesVisible(Boolean flag, ArrayList<String> places, String subTitle) {
+        int visibility = View.GONE;
         if(flag) {
             visibility = View.VISIBLE;
         }
 
-        txtSubTitle.setVisibility(visibility);
+        txtSubTitle.setText(subTitle);
+
         imgPlaces.setVisibility(visibility);
 
         // Log.d("AAAA","setPlaces");
@@ -168,13 +204,60 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // 비슷한 관광지 Click시 이벤트 처리
     public void onClick(View view) {
         TextView tmp = (TextView)view;
         String value = tmp.getText().toString();
+        if(value.equals("") || value.length() <= 0)
+            return;
 
-        // 추천지 보여준다
-        Intent recommend_intent = new Intent(getApplicationContext(), RecommendActivity.class);
-        recommend_intent.putExtra("value", value);
-        startActivity(recommend_intent);
+        Log.d("AAAA", "value : " + value);
+        setPlacesVisible(false, null, "Recommendations");
+
+        editSearch.setText(value);
+
+        Log.d("AAAA", "onClick...");
+        viewPager2.setVisibility(View.VISIBLE);
+        setRecommend_list(value);
+    }
+
+    // Flask API에서 추천 관광지 받아오기
+    public void setRecommend_list(String item) {
+        JsonObject input = new JsonObject();
+        input.addProperty("name", item);
+
+        flaskAPI.postRecommend(input).enqueue(new Callback<JsonObject>(){
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                if(response.isSuccessful()){
+                    JsonObject object = response.body();
+                    JsonArray jsonlist= object.getAsJsonArray("recommended_landmarks");
+                    Log.d("POST TEST", jsonlist.toString());
+
+                    recommend_list = new ArrayList<>();
+                    for(int i = 0; i < 5; i++)
+                        recommend_list.add(jsonlist.get(i).toString());
+                    Log.d("POST TEST", recommend_list.get(0));
+                    Log.d("POST TEST", "성공");
+
+                    ArrayList<Data> list = new ArrayList<>();
+                    list.add(new Data(android.R.color.black,recommend_list.get(0)));
+                    list.add(new Data(android.R.color.holo_red_light, recommend_list.get(1)));
+                    list.add(new Data(android.R.color.holo_green_dark, recommend_list.get(2)));
+                    list.add(new Data(android.R.color.holo_orange_dark, recommend_list.get(3)));
+                    list.add(new Data(android.R.color.holo_blue_light, recommend_list.get(4)));
+                    viewPager2.setAdapter(new ViewPagerAdapter(list));
+                }
+                else{
+                    Log.d("POST TEST", "response 실패");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Log.d("POST TEST", "실패");
+                t.printStackTrace();
+            }
+        });
     }
 }
